093157dd45d98b9894d2e8daabbbd0d4f6ddb5bd
hoon-hoon <kc0393@naver.com>
2023-03-21 11:21:31 +0900

    ExpenseMain, ExpenseTable + SettlementSummary

--- src/components/ExpenseMain.jsx
+++ src/components/ExpenseMain.jsx
@@ -1,17 +1,63 @@
+import { Col, Container, Row } from "react-bootstrap"
+import { useRecoilValue } from "recoil"
+import styled from "styled-components"
+import { AddExpenseForm } from "./AddExpenseForm"
+import { ExpenseTable } from "./ExpenseTable"
+import { groupNameState } from "../state/groupName"
+import { SettlementSummary } from "./SettlementSummary"
+
 export const ExpenseMain = () => {
     return (
-        <div>
-            Expense Main Component
-            {/* Left Pane */}
-            <div>
-                {/* TODO: 비용 추가 폼 컴포넌트 렌더링 */}
-                {/* TODO: 정산 결과 컴포넌트 렌더링 */}
-            </div>
-            {/* Right Pane */}
-            <div>
-                {/* 그룹명 헤더 렌더링 */}
-                {/* 비용 리스트 컴포넌트 렌더링 */}
-            </div>
-        </div>
+        <Container fluid>
+            <Row>
+                <Col xs={12} sm={5} md={4}>
+                    <LeftPane />
+                </Col>
+                <Col>
+                    <RightPane />
+                </Col>
+            </Row>
+        </Container>
     )
 }
+
+const LeftPane = () => (
+    <Container>
+        <Row>
+
+        </Row>
+        <Row>
+            <AddExpenseForm />
+        </Row>
+        <Row>
+            {/* TODO: 정산 결과 컴포넌트 렌더링 */}
+            <SettlementSummary />
+        </Row>
+    </Container>
+)
+
+const RightPane = () => {
+    const groupName = useRecoilValue(groupNameState)
+    return (
+        <StyledContainer>
+            <Row>
+                <StyledGroupName>{groupName || '그룹 이름'}</StyledGroupName>
+            </Row>
+            <Row>
+                <ExpenseTable />
+            </Row>
+        </StyledContainer>
+    )
+}
+
+const StyledGroupName = styled.h2`
+  margin-bottom: 80px;
+  font-weight: 700;
+  font-size: 48px;
+  line-height: 48px;
+  text-align: center;
+`
+
+const StyledContainer = styled(Container)`
+  padding: 100px 31px 100px 31px;
+`
